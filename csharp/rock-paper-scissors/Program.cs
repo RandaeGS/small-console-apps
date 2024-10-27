0633f6a231f212b32b1bc231bf51d6dbcbab137d
RandaeGS <garciasanchezrd@gmail.com>
2024-10-27 00:58:16 -0400

    rock paper scissors in csharp finished

--- csharp/rock-paper-scissors/Program.cs
+++ csharp/rock-paper-scissors/Program.cs
@@ -1,12 +1,42 @@
-﻿using System;
-
-namespace RockPaperScissors
+﻿namespace RockPaperScissors
 {
     class Program
     {
         static void Main()
         {
+            string[] options = { "rock", "paper", "scissors" };
+            Random rnd = new();
 
+            Console.WriteLine("Welcome to Rock Paper Scissors");
+            Console.WriteLine("Write your choice (rock, paper or scissors)");
+            string userChoice = Console.ReadLine().ToLower();
+
+            if (!options.Contains(userChoice))
+            {
+                Console.WriteLine("Invalid input");
+                return;
+            }
+
+            int computerIndex = rnd.Next(options.Length);
+            string computerChoice = options[computerIndex];
+
+            Console.WriteLine($"Computer chose {computerChoice}");
+
+            if (computerChoice == userChoice)
+            {
+                Console.WriteLine("It is a draw!");
+                return;
+            }
+            else if ((userChoice == "rock" && computerChoice == "scissors") ||
+                                 (userChoice == "paper" && computerChoice == "rock") ||
+                                 (userChoice == "scissors" && computerChoice == "paper"))
+            {
+                Console.WriteLine("You won!");
+            }
+            else
+            {
+                Console.WriteLine("Computer wins!");
+            }
         }
     }
 }
--- csharp/rock-paper-scissors/obj/Debug/net8.0/rock-paper-scissors.AssemblyInfo.cs
+++ csharp/rock-paper-scissors/obj/Debug/net8.0/rock-paper-scissors.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("rock-paper-scissors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33578d48c401895b726cc40578288c771fff6d2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("rock-paper-scissors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("rock-paper-scissors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
